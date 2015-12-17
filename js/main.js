6fd627dae56103ae277f619d33d5ae27a33579f3
Christina Deemer <cahdeemer@gmail.com>
2015-12-17 15:01:31 -0500

    modal structure, stylesheets

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(window).load(function(){
+        $('#myModal').modal('show');
+    });
+
+
+
+
+//////////////////////////////////
+// notes from meeting with Jeff //
+//////////////////////////////////
+
+// $(document).ready(function() {
+
+	 
+// 	 // grab all the values
+// 	 // var someVar = $('element');
+// 	 // var hasInteractedWith = false
+
+// 	 // someVar.onClick = function() {
+// 	 // 	var crossWordItem = (this).data('crossword-item');
+// 	 // 	if(!hasInteractedWith) { hasInteractedWith = true; //show continue button
+// 	 // 	}
+// 	 // }
+
+// 	 // // how to call JSON file
+// 	 // $getJSON('file.json', function(data) {
+// 	 // 	console.log(data);
+// 	 // })  
+
+// });
