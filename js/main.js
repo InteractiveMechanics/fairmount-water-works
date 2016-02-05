9ec37d783c98786fc4d9ccc19d2285681afcd210
Christina Deemer <cahdeemer@gmail.com>
2016-02-05 14:35:32 -0500

    organizing/refactoring js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,53 @@
-/*** Handle jQuery plugin naming conflict between jQuery UI and Bootstrap ***/
-$.widget.bridge('uibutton', $.ui.button);
-$.widget.bridge('uitooltip', $.ui.tooltip);
+ $(document).ready(function(event) {
+    
+    //homepage
+    introModal();
+    checkForActiveButtons();
+    tallyScore();
+    completionModal();
+    gameRestart();
 
+    //activity 1: crossword
+    crosswordModal();
+    crosswordLogic();
+    showCrosswordAnswer();
 
-//show modal only once
-$(document).ready(function(event) {
-    if(localStorage.getItem('modal_content_intro') != 'show'){
+    //activity 2: drag-and-drop
+    watercycle();
+
+    //activity 3: no js
+
+    //activity 4: video
+    waterwheel();
+
+    //activity 5: click to reveal
+    pollution();
+
+    // get JSON for activity 2
+    getCrosswordTrivia();
+
+}); 
+
+ 
+// hompepage intro modal show only once
+function introModal() {
+   if(localStorage.getItem('modal_content_intro') != 'show'){
         $('#myModal').modal('show');
         localStorage.setItem('modal_content_intro','show');
     }
+}
 
-  checkForActiveButtons();
-  watercycle();
-  tallyScore();
-  pollution();
-}); 
+
+// homepage: if all activities are complete, completion modal displays
+  function completionModal(){  
+    if (!$('.game-btn').not('.game-btn-complete').length) {
+      $('#myModal').modal('show');
+        $('.modal-content-intro').hide();
+        $('.modal-content-completion').removeClass('hidden');
+        event.preventDefault(); 
+    }
+  }
+
 
 //homepage 
 function checkForActiveButtons() {
@@ -75,16 +108,6 @@ $('.continue-btn-game-5').click(function() {
 })
 
 
-// homepage: if all buttons are green, completion modal displays
-$(function(){
-		if (!$('.game-btn').not('.game-btn-complete').length) {
-			$('#myModal').modal('show');
-    		$('.modal-content-intro').hide();
-    		$('.modal-content-completion').removeClass('hidden');
-        event.preventDefault();	
-		}
-	});
-
 // hompeage: progress meter
 function tallyScore() {
   var score = $('.game-btn-complete').length;
@@ -92,98 +115,105 @@ function tallyScore() {
 };
 
 
-// invisibile page reload on desktop
-$('.magic-button').mousedown(function(e) {
-    clearTimeout(this.downTimer);
-    this.downTimer = setTimeout(function() {
-        // alert('mousedown > 3 sec');
-        location.reload();
-        localStorage.clear('.modal-content-intro');   
-    }, 3000);
-}).mouseup(function(e) {
-    clearTimeout(this.downTimer);
-});
+function gameRestart() {
+  // invisibile page reload on desktop
+  $('.magic-button').mousedown(function(e) {
+      clearTimeout(this.downTimer);
+      this.downTimer = setTimeout(function() {
+          // alert('mousedown > 3 sec');
+          location.reload();
+          localStorage.clear('.modal-content-intro');   
+      }, 3000);
+  }).mouseup(function(e) {
+      clearTimeout(this.downTimer);
+  });
 
-//invisible page reload on touch 
-$('.magic-button').bind('touchstart', function(e) {
-    clearTimeout(this.downTimer);
-    this.downTimer = setTimeout(function() {
-        // alert('mousedown > 3 sec');
-        location.reload();
-        localStorage.clear('.modal-content-intro');   
-    }, 3000);
-}).bind('touchend', function(e) {
-    clearTimeout(this.downTimer);
-});
+  //invisible page reload on touch 
+  $('.magic-button').bind('touchstart', function(e) {
+      clearTimeout(this.downTimer);
+      this.downTimer = setTimeout(function() {
+          // alert('mousedown > 3 sec');
+          location.reload();
+          localStorage.clear('.modal-content-intro');   
+      }, 3000);
+  }).bind('touchend', function(e) {
+      clearTimeout(this.downTimer);
+  });
+}
 
+function crosswordModal() {
+  $('[data-toggle=tooltip]').tooltip({trigger: 'click', html: true}).tooltip('show');
 
+  //crossword modal shows when user clicks on tooltip
+    $('[role=tooltip]').click(function(event) {
+        var clue = $(this)[0].textContent;
+        $('.modal-content').html($('.clue-' + clue).html());
+        $(this).attr('data-toggle', 'modal').attr('data-target', '#crosswordModal');
+        $(this).addClass('disabled');
+    });
+}
 
-// crossword tooltips show on page load
-$('[data-toggle=tooltip]').tooltip({trigger: 'click', html: true}).tooltip('show');
-
-//crossword modal shows when user clicks on tooltip
-$('[role=tooltip]').click(function(event) {
-    var clue = $(this)[0].textContent;
-    $('.modal-content').html($('.clue-' + clue).html());
-    $(this).attr('data-toggle', 'modal').attr('data-target', '#crosswordModal');
-    $(this).addClass('disabled');
-});
-
-//crossword answer visible when next button clicked
-$('#crosswordModal').on('hidden.bs.modal', function() {
-    var $crosswordAnswer = $('.crossword-answer');
-    var $modalClueClassName = $('.clueNumber:first').text();
-    var $modalClueName = $modalClueClassName.substr(1);
-    var $crosswordClueClass = ".crossword-clue-" + $modalClueName;
-    
-
-    $($crosswordClueClass).find('>:first-child').removeClass('crossword-clue-hidden');
-    $($crosswordClueClass).siblings('.tooltip').children('.tooltip-inner').addClass('tooltip-correct');
-    $($crosswordClueClass).siblings('.top').children('.tooltip-arrow').addClass('tooltip-top-arrow-correct');
-    $($crosswordClueClass).siblings('.right').children('.tooltip-arrow').addClass('tooltip-right-arrow-correct');
-    $($crosswordClueClass).siblings('.left').children('.tooltip-arrow').addClass('tooltip-left-arrow-correct');
-
-    $($crosswordClueClass).tooltip('show')
-      .attr('data-original-title', '<img src="img/icons/icon-correct2.svg">')
-      .tooltip('fixTitle')
-      .tooltip('show')
-});
-
-// handlebars - crossword trivia
-$(function () {
-  var raw_template = $('#trivia-template').html();   
-  var template = Handlebars.compile(raw_template);
-  var placeHolder = $(".modal-data");
-
-  $.get("./data/crossword.json",function(data,status,xhr){
-        $.each(data,function(index,element){
-            console.log(data);
+function showCrosswordAnswer() {
+  $('#crosswordModal').on('hidden.bs.modal', function() {
+        var $crosswordAnswer = $('.crossword-answer');
+        var $modalClueClassName = $('.clueNumber:first').text();
+        var $modalClueName = $modalClueClassName.substr(1);
+        var $crosswordClueClass = ".crossword-clue-" + $modalClueName;
         
-          var html = template(data);
-          // Render the posts into the page
-          placeHolder.append(html);
-        });
-      }); 
-});
 
-// logic for trivia questions
-$(document).delegate(".modal-trivia-button", "click", function(event){
-    var button = $(this).text().trim();
-    var answer = $('.answer').html().trim();
-    if (button == answer) {
-      $(this).addClass('correct-answer');
-      $(this).append('<img class="trivia-correct icon-correct" src="../img/icons/icon-correct-green.svg" alt="correct icon">');
-      $(this).parents('.options').siblings('.modal-trivia-next-button').attr('disabled', false);
-    } else {
-      $(this).addClass('disabled');
-      alert('close - try again!');
-    }   
- });
+        $($crosswordClueClass).find('>:first-child').removeClass('crossword-clue-hidden');
+        $($crosswordClueClass).siblings('.tooltip').children('.tooltip-inner').addClass('tooltip-correct');
+        $($crosswordClueClass).siblings('.top').children('.tooltip-arrow').addClass('tooltip-top-arrow-correct');
+        $($crosswordClueClass).siblings('.right').children('.tooltip-arrow').addClass('tooltip-right-arrow-correct');
+        $($crosswordClueClass).siblings('.left').children('.tooltip-arrow').addClass('tooltip-left-arrow-correct');
+
+        $($crosswordClueClass).tooltip('show')
+          .attr('data-original-title', '<img src="img/icons/icon-correct2.svg">')
+          .tooltip('fixTitle')
+          .tooltip('show')
+    });   
+}
+
+//handlebars 
+function getCrosswordTrivia() {
+      var raw_template = $('#trivia-template').html();   
+      var template = Handlebars.compile(raw_template);
+      var placeHolder = $(".modal-data");
+
+      $.get("./data/crossword.json",function(data,status,xhr){
+            $.each(data,function(index,element){
+                console.log(data);
+            
+              var html = template(data);
+              // Render the posts into the page
+              placeHolder.append(html);
+            });
+      }); 
+} 
+
+
+function crosswordLogic() {
+   // crossword: logic for trivia questions
+    $(document).delegate(".modal-trivia-button", "click", function(event){
+        var button = $(this).text().trim();
+        var answer = $('.answer').html().trim();
+        if (button == answer) {
+          $(this).addClass('correct-answer');
+          $(this).append('<img class="trivia-icon icon-correct" src="../img/icons/icon-correct-green.svg" alt="correct icon">');
+          $(this).parents('.options').siblings('.modal-trivia-next-button').attr('disabled', false);
+        } else {
+          $(this).addClass('disabled');
+          $(this).append('<img class="trivia-icon icon-incorrect" src="../img/icons/icon-incorrect-red.svg" alt="incorrect icon">');
+        }   
+     });
+}
 
 
 // watercycle - drag and drop
-
 function watercycle() {
+   /*** Handle jQuery plugin naming conflict between jQuery UI and Bootstrap ***/
+    $.widget.bridge('uibutton', $.ui.button);
+    $.widget.bridge('uitooltip', $.ui.tooltip);
 
   $('.draggable-widget').draggable({ 
     snap: '.droppable-widget', 
@@ -206,32 +236,30 @@ function watercycle() {
           } else {
           ui.draggable.append('<img class="watercycle-icon icon-incorrect" src="../img/icons/icon-incorrect-red.svg" alt="incorrect icon">')
           ui.draggable.children(":nth-child(2)").addClass('hidden');
-            // ui.draggable.draggable('option', 'revert', true);
-            // ui.draggable.children(":nth-child(2)").removeClass('hidden');
-            // ui.draggable.find('.icon-incorrect').remove();
+
           }
         }
       }
   });
-};
+}
 
-$('.waterwheel-button').click(function() {
-    alert('your function is working!');
-    $(this).addClass('hidden');
-    $('#myVideo').get(0).play();
-    $('.waterwheel-header').addClass('hidden');
-    $('.continue-btn-game-4').removeClass('hidden');
-});
+function waterwheel() {
+  // waterwheel on desktop
+  $('.waterwheel-button').click(function() {
+      $(this).addClass('hidden');
+      $('#myVideo').get(0).play();
+      $('.waterwheel-header').addClass('hidden');
+      $('.continue-btn-game-4').removeClass('hidden');
+  });
 
-$('.waterwheel-button').bind('touchstart', function(e) {
-    alert('your function is working!');
-    $(this).addClass('hidden');
-    $('#myVideo').get(0).play();
-    $('.waterwheel-header').addClass('hidden');
-    $('.continue-btn-game-4').removeClass('hidden');
-});
-
-
+  // waterwheel on touch devices
+  $('.waterwheel-button').bind('touchstart', function(e) {
+      $(this).addClass('hidden');
+      $('#myVideo').get(0).play();
+      $('.waterwheel-header').addClass('hidden');
+      $('.continue-btn-game-4').removeClass('hidden');
+  });
+}
 
 function pollution() {
   $('.pollution-element').click(function() {
